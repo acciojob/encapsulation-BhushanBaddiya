fdd7e82b85cc995bb7819a8d497beb0fdafdd68b
bhushan baddiya <bhushanbaddiya0@gmail.com>
2024-02-10 17:17:29 +0530

    changed

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+    //As we know name is private in class RWOnly so it doesnt excess out side
+    //the class it only excess inside the class only so giving error
+    //
+    private String name;
+//    RWOnly(String name ){
+//        this.name = name;
+//    }
+    public void setter(String name){
+        this.name = name;
+    }
+    public String getter(){
+        return name;
+    }
+
 }
